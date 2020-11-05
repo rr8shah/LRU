c1a24bd1a0d59d33b83ebdf6ae4ab7307a1aba77
rr8shah <rr8shah@uwaterloo.ca>
2020-11-05 02:10:56 -0500

    example

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
 from LRU import LRUCache
-import time
-
 '''
 This code creates the cache memory and executes 
 some of the methods for demonstration purpose
@@ -8,7 +6,6 @@ some of the methods for demonstration purpose
 '''
 
 cache = LRUCache(2)
-start = time.time()
 cache.put(1, 1)
 cache.put(2, 2)
 cache.get(1)
@@ -26,5 +23,3 @@ cache.get(4)
 cache.reset()
 cache._print()
 cache.get(1)
-end = time.time()
-print('Time taken to execute', end - start, 'seconds')
