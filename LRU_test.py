04c1d7c6c3242553ead06a7f18d13a187e364179
rr8shah <rr8shah@uwaterloo.ca>
2020-11-04 23:50:45 -0500

    Unit Test

--- LRU_test.py
+++ LRU_test.py
@@ -18,7 +18,7 @@ class TestLRU(unittest.TestCase):
     def test_put(self):
         cache = LRUCache(2)
         cache.put(1,'One')
-        result = len(cache.dict)
+        result = len(cache.cache)
         self.assertEqual(result, 1, 'Put test Passed')
     
     def test_delete(self):
@@ -26,7 +26,7 @@ class TestLRU(unittest.TestCase):
         cache.put(1,'One')
         cache.put(2,'Two')
         cache._del(1)
-        result = len(cache.dict)
+        result = len(cache.cache)
         self.assertEqual(result, 1, 'Delete test Passed')
         
     def test_reset(self):
@@ -34,7 +34,7 @@ class TestLRU(unittest.TestCase):
         cache.put(1,'One')
         cache.put(2,'Two')
         cache.reset()
-        result = len(cache.dict)
+        result = len(cache.cache)
         self.assertEqual(result, 0, 'Reset test Passed')
     
     def test_capacity_exceed(self):
@@ -44,10 +44,10 @@ class TestLRU(unittest.TestCase):
         cache.get(1)
         cache.put(2,4)
         cache.put(3, 3)
-        result = 1 not in cache.list
-        result2 = len(cache.dict)
-        self.assertEqual(result, True, 'Reset test Passed')
-        self.assertEqual(result2, 2, 'Reset test Passed')
+        result = 1 not in cache.keys
+        result2 = len(cache.cache)
+        self.assertEqual(result, True, 'Least Recently used memory removal test Passed')
+        self.assertEqual(result2, 2, 'Maximum capacity not exceeded test Passed')
         
 if __name__ == '__main__':
     unittest.main()
